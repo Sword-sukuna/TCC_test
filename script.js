21d3e896a4e0d255b4305ab6b214abe7195bc19a
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-04-10 16:21:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const API = "https://tcc-alunos-api.vercel.app/api/alunos";
 
+// cadastrar
 document.getElementById("alunoForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
@@ -20,14 +21,23 @@ document.getElementById("alunoForm").addEventListener("submit", async (e) => {
 
   const data = await res.json();
   alert(data.message);
+
+  buscarAlunos();
 });
 
+// buscar
 async function buscarAlunos() {
   const nome = document.getElementById("buscar").value;
   const res = await fetch(`${API}?nome=${nome}`);
   const alunos = await res.json();
 
   document.getElementById("lista").innerHTML = alunos
-    .map(a => `<p>${a.nome} - ${a.curso}</p>`)
+    .map(a => `
+      <p>
+        <strong>${a.nome}</strong><br>
+        ${a.curso || "Sem curso"}<br>
+        ${a.email || ""}
+      </p>
+    `)
     .join("");
 }
